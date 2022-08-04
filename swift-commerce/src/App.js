36d899c84e760b42d70a78e4bf4a04b7f8b60916
kevinmbuicode <kevinmushan@gmail.com>
2022-08-04 23:03:22 +0300

    products fetched rendered sucessfully

---
+++ swift-commerce/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Home from './components/Home';
+import NavBar from './components/NavBar';
+import Products from './components/Products';
+
+function App() {
+  return (
+    <>
+      <NavBar/>
+      <Home/>
+      <Products/>
+    </>
+  );
+}
+
+export default App;
---
+++ swift-commerce/src/components/Home.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import background from "../assets/bground.jpg";
+
+const Home = () => {
+  return (
+    <div className="hero">
+      <div className="card bg-dark text-white border-0">
+        <img
+          src={background}
+          className="card-img"
+          alt="background_image"
+          height="550px"
+        />
+        <div className="card-img-overlay">
+          <h5 className="card-title display-3 fw-bolder mb-0">NEW ARRIVALS</h5>
+          <p className="card-text lead fs-2">
+            CHECK OUT ALL THE TRENDS
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
